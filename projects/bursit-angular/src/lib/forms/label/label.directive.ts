c5673c6127b1184f11a67934a49a4315ed497798
Gustavo Petruzzi <gustavopetruzzi19@gmail.com>
2026-07-31 15:06:17 +0000

    feat: add for/id if not set by the user

--- projects/bursit-angular/src/lib/forms/label/label.directive.ts
+++ projects/bursit-angular/src/lib/forms/label/label.directive.ts
@@ -10,9 +10,16 @@ export class LabelDirective implements OnInit {
   private readonly _fieldId = inject(FORM_FIELD_ID, { optional: true });
 
   ngOnInit(): void {
-    const userSet = this._el.nativeElement.getAttribute('for');
-    if (!userSet && this._fieldId) {
-      this._el.nativeElement.setAttribute('for', this._fieldId);
+    if (this._fieldId) {
+      const userSetFor = this._el.nativeElement.getAttribute('for');
+      if (!userSetFor) {
+        this._el.nativeElement.setAttribute('for', this._fieldId);
+      }
+
+      const userSetId = this._el.nativeElement.getAttribute('id');
+      if (!userSetId) {
+        this._el.nativeElement.setAttribute('id', `${this._fieldId}-label`);
+      }
     }
   }
 }
